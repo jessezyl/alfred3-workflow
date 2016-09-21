600389b40d6a2f118d94a832b72ad40bb2d6ea6f
张云龙 <jesse.zyl@gmail.com>
2016-09-21 15:02:16 +0800

    update performance

--- getNewMovie/config.py
+++ getNewMovie/config.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# config_new.py.py created by Jesse
+# config.py created by Jesse
 # 16/9/20 15:29
 
 
 from workflow import web
 import os
+import datetime
 import json
 
 coming_soon_url = 'https://api.douban.com/v2/movie/coming_soon'
 nowplaying_url = 'https://api.douban.com/v2/movie/in_theaters'
 
 
-def get_movies(url):
-    data = web.get(url).json()
+def get_data(arg):
+    url = arg_switch(arg)
+    data_filename = datetime.date.today().strftime('%Y_%m_%d') + '_%s.json' % arg
+    if not os.path.exists(data_filename):
+        web.get(url).save_to_path(data_filename)
+    data_fd = open(data_filename, 'r')
+    data = json.load(data_fd)
+    data_fd.close()
+    return data
+
+
+def get_movies(arg):
+    data = get_data(arg)
     movie_item = {}
     movie_list = []
-
-    def add_name(stuff):
-        return stuff['name']
 
     for movie_info in data['subjects']:
         movie_item['title'] = movie_info['title']
         icon = movie_info['id'] + '.jpg'
         if not os.path.exists(icon):
             web.get(movie_info['images']['small']).save_to_path(icon)
-        directors_name = map(add_name, movie_info['directors'])
-        actors_name = map(add_name, movie_info['casts'])
+        directors_name = map(lambda x: x['name'], movie_info['directors'])
+        actors_name = map(lambda x: x['name'], movie_info['casts'])
 
         if movie_info['rating']['average'] == 0:
             score = u'暂无评分'
@@ -53,7 +62,7 @@ def main(wf):
     else:
         arg = 'now'
 
-    wf_items = get_movies(arg_switch(arg))
+    wf_items = get_movies(arg)
 
     for item in wf_items:
         wf.add_item(title=item['title'], subtitle=item['subtitle'], arg=item['arg'], icon=item['icon'], valid=True)
